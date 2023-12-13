2a1e2ec17b502da8ed65c8637e4917e9e63f5e82
firas hamila <firas.hm.1996@gmail.com>
2023-12-13 09:19:30 +0100

    Ajoutez des fichiers projet.

---
+++ LibraryAPI/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        [Range(0,5)]
+        public double Rating { get; set; }
+        [Url]
+        public string? CoverUrl { get; set; }
+    }
+}
---
+++ LibraryAPI/Models/LibraryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Book 1", Description = "Description 1", Author = "Author 1", Rating = 4.5, CoverUrl = "https://example.com/book1" },
+                new Book { Id = 2, Title = "Book 2", Description = "Description 2", Author = "Author 2", Rating = 3.8, CoverUrl = "https://example.com/book2" },
+                new Book { Id = 3, Title = "Book 3", Description = "Description 3", Author = "Author 3", Rating = 4.2, CoverUrl = null }
+                );
+        }
+    }
+}
