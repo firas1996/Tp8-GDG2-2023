8e9fa8986daaf49772b0b9f010d1126206e2196e
firas hamila <firas.hm.1996@gmail.com>
2023-12-13 09:26:31 +0100

    ss

---
+++ LibraryAPI/Migrations/20231213082301_WithData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class WithData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CoverUrl", "Description", "Rating", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Author 1", "https://example.com/book1", "Description 1", 4.5, "Book 1" },
+                    { 2, "Author 2", "https://example.com/book2", "Description 2", 3.7999999999999998, "Book 2" },
+                    { 3, "Author 3", null, "Description 3", 4.2000000000000002, "Book 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
